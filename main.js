db0d4c4fec21ad28339e2fb1a0346e58a5d1c258
Moustachaus <moustachaus@gmail.com>
2019-01-25 16:46:43 -0500

    aaa

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NTM4MTEzNzc4MTM5NjYwMjkz.DyvcKg.M3ZorM6G64M9zKqUtL2zJ-xoSnA';
 
 bot.on('ready', () => {
     console.log("Bot Ready");
