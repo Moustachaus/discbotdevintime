08238dfb024fe293638288ae1b00cf6a955759fc
Moustachaus <moustachaus@gmail.com>
2019-01-25 16:35:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
 const token = 'NTM4MTEzNzc4MTM5NjYwMjkz.DyvcKg.M3ZorM6G64M9zKqUtL2zJ-xoSnA';
 
 var bot = new Discord.Client();
